1204526bfa39f8b9ac1cbe34868f6feb0e4ca420
Stacey Andrew Gonzaga <andrew23.gonzaga@gmail.com>
2023-10-12 21:20:50 +0800

    Add project files.

---
+++ InvenTrack/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using InvenTrack.Model;
+using InvenTrack.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InvenTrack.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get { return _currentUserAccount; }
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount.Username = user.UserName;
+                CurrentUserAccount.DisplayName = $"Hello, {user.Name} {user.LastName}!";
+                CurrentUserAccount.ProfilePicture = null;
+            } 
+            else
+            {
+                CurrentUserAccount.DisplayName = "Invalid user, not logged in";
+            }
+        }
+    }
+}
